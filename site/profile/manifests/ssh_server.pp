03e35d1617027b9170d8653ab45d58d5994ba611
FahmyKhalilM <72336661+FahmyKhalilM@users.noreply.github.com>
2021-09-17 18:43:48 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDdZNDlYJDy0mqF9KP343mIanOuKms9oMZVoMAWr4cHK08ZHlTsFeBQOq2/3iupeIKTlR1SAFowoUrxb4Ki2zRD5LV+rvilKq+BhEQhUaJrAqQqhT/6rcwiL/M25Cg3th8TF3L9CPFAePtDOS2ueov6Yng+xRTB2JFzOW27uM55Zx+ApwmkryAOa1lZeXZDnYSqNJ7nP0xEB6RkhH8awZ6UrnIn8q0qQGLEBf0qvGZCaM+xSrbTaPrJnjKaM1c7LbkguBybZMN2b6Ju0nWK0l2HQdFNlbMEvXd5S/6A2u1aEuUTOHa5ruyDoV/SAnaPGaaEeXkaI5+XBTJxPmfE72BV',
   }  
 }
