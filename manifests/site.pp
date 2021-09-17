06d3da6fcaa470183659d72693c50410b3ed39f7
FahmyKhalilM <72336661+FahmyKhalilM@users.noreply.github.com>
2021-09-17 17:46:58 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,9 @@ node default {
 }
 node 'master.puppet.vm' {
 include role::master_server
+file { '/root/README':
+  ensure =>file,
+  content =>$fqdn,
 }
 node 'web.puppet.vm' {
 include role::app_server
